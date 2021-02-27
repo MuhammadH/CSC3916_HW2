d935413b287b3da76a88da662cd3d172bcaec8e7
MuhammadH <Muhammadhauris@gmail.com>
2021-02-27 03:34:16 -0700

    server

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ router.get('/movies',function(req, res) {
             res = res.type(req.get('Content-Type'));
         }
         var req_obj = getJSONObject(req);
-        res.json({success: true, msg: 'did a thing', headers: o.headers, query: req.query, host: o.key});
+        res.json({success: true, msg: 'did a thing', headers: req_obj.headers, query: req.query, host: req_obj.key});
     }
 );
 
